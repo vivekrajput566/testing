c02d851c0092d69c4d71b49863d40ea6cd004118
vivekrajput566 <rajputvivek566@gmail.com>
2018-08-10 13:09:26 +0530

    changes in syntax

--- form_post.js
+++ form_post.js
@@ -1,37 +1,28 @@
 const express = require('express');
-const bodyParser=require('body-parser');
+const file_system = require('fs');
+const bodyParser = require('body-parser');
+const PORT = 8000;
 const app=express();
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/data',function(req,res,err){
+const display_form_data = function(req,res){
   res.writeHead(200,{"Content-type":"text/html"});
-  res.write('your name is '+req.body.name+'' );
+res.write(`your name is ${req.body.name}` );
   res.write('</br>');
-  res.write('your AGE is '+req.body.age+'' );
+  res.write(`your AGE is ${req.body.age}` );
   res.write('</br>');
-  res.write('your email id is '+req.body.email_id+'' );
+  res.write(`your email id is ${req.body.email_id}` );
   res.end();
-});
-
-app.get('/',function(req,res){
-  res.writeHead(200,{"Content-type":"text/html"});
-  // res.write('<form action="/data" method="post">');
-  // res.write('<h1>');
-  // res.write('ENTER YOUR NAME');
-  // res.write('<input type="text" name="name">');
-  // res.write('</br>');
-  // res.write('ENTER YOUR AGE');
-  // res.write('<input type="number" name="age">');
-  // res.write('</br>');
-  // res.write('ENTER YOUR EMAIL ID');
-  // res.write('<input type="text" name="email_id">');
-  // res.write('<input type="submit">');
-  // res.write('<h1>');
-  // res.write('</form>');
-  res.write('./form_get.html');
+}
+const display_form = function(req,res){
+	const _form = file_system.readFileSync('./form_post.html');
+res.write(_form);
   res.end();
-});
 
-app.listen('8000');
+	}
+app.post('/data',display_form_data);
+
+app.get('/',display_form);
+
+app.listen(PORT,() => console.log(`Server is listening at ${PORT}`));
